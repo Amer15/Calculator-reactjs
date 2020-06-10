620efd850dfc6a2ad7fdcaf46388ded7cb79b165
AMER <amer95521@gmail.com>
2020-06-10 14:10:41 +0530

    add all files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,118 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Button from './components/Button';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      result: ""
+    }
+
+  }
+
+  // functions - calculate , clear/reset, delete
+  calculate = () => {
+    try {
+      this.setState({
+        result: eval(this.state.result)
+      })
+    } catch (e) {
+      this.setState({
+        result: "error"
+      })
+
+    }
+  };
+
+  reset = () => {
+    this.setState({
+      result: ""
+    })
+  };
+
+  backspace = () => {
+    this.setState({
+      result: this.state.result.slice(0, -1)
+    })
+  };
+
+
+  onClickHandler(e) {
+    // console.log(e.target.innerText);
+    let button;
+    if (e.target.innerText === "x") {
+      button = "*";
+    }
+    else if (e.target.innerText === "÷") {
+      button = "/";
+    }
+    else {
+      button = e.target.innerText;
+    }
+
+    if (button === "=") {
+      this.calculate();
+    }
+    else if (button === "CE") {
+      this.reset();
+    }
+    else if (button === "C") {
+      this.backspace();
+    }
+    else {
+      this.setState({
+        result: this.state.result + button
+      });
+    }
+
+  };
+
+
+
+  render() {
+    return (
+      <div className="main-container">
+        <div className="calculator-container">
+          <div className="result-container">{this.state.result}</div>
+          <div className="controls-container">
+            <div className="row">
+              <Button onclick={e => this.onClickHandler(e)}>CE</Button>
+              <Button onclick={e => this.onClickHandler(e)}>(</Button>
+              <Button onclick={e => this.onClickHandler(e)}>)</Button>
+              <Button onclick={e => this.onClickHandler(e)}>C</Button>
+            </div>
+            <div className="row">
+              <Button onclick={e => this.onClickHandler(e)}>1</Button>
+              <Button onclick={e => this.onClickHandler(e)}>2</Button>
+              <Button onclick={e => this.onClickHandler(e)}>3</Button>
+              <Button onclick={e => this.onClickHandler(e)}>+</Button>
+            </div>
+            <div className="row">
+              <Button onclick={e => this.onClickHandler(e)}>4</Button>
+              <Button onclick={e => this.onClickHandler(e)}>5</Button>
+              <Button onclick={e => this.onClickHandler(e)}>6</Button>
+              <Button onclick={e => this.onClickHandler(e)}>-</Button>
+            </div>
+            <div className="row">
+              <Button onclick={e => this.onClickHandler(e)}>7</Button>
+              <Button onclick={e => this.onClickHandler(e)}>8</Button>
+              <Button onclick={e => this.onClickHandler(e)}>9</Button>
+              <Button onclick={e => this.onClickHandler(e)}>x</Button>
+            </div>
+            <div className="row">
+              <Button onclick={e => this.onClickHandler(e)}>.</Button>
+              <Button onclick={e => this.onClickHandler(e)}>0</Button>
+              <Button onclick={e => this.onClickHandler(e)}>=</Button>
+              <Button onclick={e => this.onClickHandler(e)}>÷</Button>
+            </div>
+          </div>
+        </div>
+      </div>
+   );
+  }
 }
 
 export default App;
